9dfd530ebdae7f440bee531afa793f55aa1f7470
Tony Tran <thanhtan.tran@gmail.com>
2025-05-08 11:07:21 +0700

    update script fix freq

--- fix_freq_rk3576.sh
+++ fix_freq_rk3576.sh
@@ -10,33 +10,30 @@ echo 1 > /sys/devices/system/cpu/cpu6/cpuidle/state1/disable
 echo 1 > /sys/devices/system/cpu/cpu7/cpuidle/state1/disable
 
 echo "NPU available frequencies:"
-cat /sys/class/devfreq/fdab0000.npu/available_frequencies
+cat /sys/class/devfreq/27700000.npu/available_frequencies
 echo "Fix NPU max frequency:"
-echo userspace > /sys/class/devfreq/fdab0000.npu/governor
-echo 1000000000 > /sys/class/devfreq/fdab0000.npu/userspace/set_freq
-cat /sys/class/devfreq/fdab0000.npu/cur_freq
+echo userspace > /sys/class/devfreq/27700000.npu/governor
+echo 1000000000 > /sys/class/devfreq/27700000.npu/userspace/set_freq
+cat /sys/class/devfreq/27700000.npu/cur_freq
 
 echo "CPU available frequencies:"
 cat /sys/devices/system/cpu/cpufreq/policy0/scaling_available_frequencies
 cat /sys/devices/system/cpu/cpufreq/policy4/scaling_available_frequencies
-cat /sys/devices/system/cpu/cpufreq/policy6/scaling_available_frequencies
 echo "Fix CPU max frequency:"
 echo userspace > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-echo 1800000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_setspeed
+echo 2208000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_setspeed
 cat /sys/devices/system/cpu/cpufreq/policy0/scaling_cur_freq
 echo userspace > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
-echo 2352000 > /sys/devices/system/cpu/cpufreq/policy4/scaling_setspeed
+echo 2304000 > /sys/devices/system/cpu/cpufreq/policy4/scaling_setspeed
 cat /sys/devices/system/cpu/cpufreq/policy4/scaling_cur_freq
-echo userspace > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor
-echo 2352000 > /sys/devices/system/cpu/cpufreq/policy6/scaling_setspeed
-cat /sys/devices/system/cpu/cpufreq/policy6/scaling_cur_freq
 
 echo "GPU available frequencies:"
-cat /sys/class/devfreq/fb000000.gpu/available_frequencies
+cat /sys/class/devfreq/27800000.gpu/cur_freq
+cat /sys/class/devfreq/27800000.gpu/available_frequencies
 echo "Fix GPU max frequency:"
-echo userspace > /sys/class/devfreq/fb000000.gpu/governor
-echo 1000000000 > /sys/class/devfreq/fb000000.gpu/userspace/set_freq
-cat /sys/class/devfreq/fb000000.gpu/cur_freq
+echo userspace > /sys/class/devfreq/27800000.gpu/governor
+echo 950000000 > /sys/class/devfreq/27800000.gpu/userspace/set_freq
+cat /sys/class/devfreq/27800000.gpu/cur_freq
 
 echo "DDR available frequencies:"
 cat /sys/class/devfreq/dmc/available_frequencies
